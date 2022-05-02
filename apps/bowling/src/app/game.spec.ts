7251be7035c2969f1f2a651f1fb43c581c7fd471
KevinYang <chgc.tw@gmail.com>
2022-05-02 08:54:34 +0800

    refactor duplicate code 1/2

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,23 +1,31 @@
 import { Game } from './game';
 
 describe('Game', () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it('should create an instance', () => {
-    expect(new Game()).toBeTruthy();
+    expect(game).toBeTruthy();
   });
 
   it('should return score of 0', () => {
-    const game = new Game();
-    for (let i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    rollMany(20, 0);
     expect(game.score).toBe(0);
   });
 
   it('should return score of 20', () => {
-    const game = new Game();
     for (let i = 0; i < 20; i++) {
       game.roll(1);
     }
     expect(game.score).toBe(20);
   });
+
+  function rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
 });
