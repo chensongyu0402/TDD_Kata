473ebdefc43c858d198a6133ea34a966af731d5b
KevinYang <chgc.tw@gmail.com>
2022-05-02 09:05:58 +0800

    third test success test 2/2

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -7,8 +7,14 @@ export class Game {
 
   get score() {
     let score = 0;
-    for (let i = 0; i < this.rolls.length; i++) {
-      score += this.rolls[i];
+    let i = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.rolls[i] + this.rolls[i + 1] === 10) {
+        score += 10 + this.rolls[i + 2];
+      } else {
+        score += this.rolls[i] + this.rolls[i + 1];
+      }
+      i += 2;
     }
     return score;
   }
