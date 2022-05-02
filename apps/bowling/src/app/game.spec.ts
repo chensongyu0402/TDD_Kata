5e509b00a3db0953c910e6f6edae266219819d22
KevinYang <chgc.tw@gmail.com>
2022-05-02 08:50:40 +0800

    second fail test

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -12,4 +12,12 @@ describe('Game', () => {
     }
     expect(game.score).toBe(0);
   });
+
+  it('should return score of 20', () => {
+    const game = new Game();
+    for (let i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score).toBe(20);
+  });
 });
